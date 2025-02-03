15a4cc1f345c435c1aadb4966eaf066dd503538a
acatarinaoaraujo <acoa_@hotmail.com>
2025-02-02 17:17:35 -1000

    final style changes to sidebar and update api url

--- eco-forest/Backend/Controllers/IMFController.cs
+++ eco-forest/Backend/Controllers/IMFController.cs
@@ -11,7 +11,7 @@ public class IMFController : ControllerBase
     public async Task<IActionResult> GetData()
     {
         var client = new HttpClient();
-        var response = await client.GetStringAsync("https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Indicator_4/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json");
+        var response = await client.GetStringAsync("https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Indicator_4/FeatureServer/0/query?where=1%3D1&outFields=Country,Technology,Unit,F2000,F2001,F2002,F2003,F2004,F2005,F2006,F2007,F2008,F2009,F2010,F2011,F2012,F2013,F2014,F2015,F2016,F2017,F2018,F2019,F2020,F2021,F2022,F2023&outSR=4326&f=json");
         var data = JsonSerializer.Deserialize<object>(response); // Adjust type as needed
         return Ok(data);
     }
