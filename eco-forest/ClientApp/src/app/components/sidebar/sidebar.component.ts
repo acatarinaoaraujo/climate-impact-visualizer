14384de20659eb40fb388b0edeeb63c3063e70df
acatarinaoaraujo <acoa_@hotmail.com>
2025-02-02 14:13:09 -1000

    add pagination into list, filter and forms

--- eco-forest/ClientApp/src/app/components/sidebar/sidebar.component.ts
+++ eco-forest/ClientApp/src/app/components/sidebar/sidebar.component.ts
@@ -3,12 +3,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 import { SidebarListComponent } from './sidebar-list/sidebar-list.component';
 
 interface Food {
@@ -26,12 +26,26 @@ interface Car {
   standalone: true,
   templateUrl: 'sidebar.component.html',
   styleUrls: ['sidebar.component.css'],
-  imports: [MatSidenavModule, MatButtonModule, MatIconModule, CommonModule, MatSliderModule, MatDividerModule, MatInputModule, MatSelectModule, MatFormFieldModule, FormsModule, SidebarListComponent],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    CommonModule,
+    MatSliderModule,
+    MatDividerModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    SidebarListComponent,
+  ],
 })
 export class SidebarComponent {
   isCollapsed = false;
   selectedValue?: string;
   selectedCar?: string;
+  startValue: number = 0;
+  endValue: number = 100000;
 
   toggleSidebar() {
     this.isCollapsed = !this.isCollapsed;
@@ -41,19 +55,18 @@ export class SidebarComponent {
     if (value >= 1000) {
       return Math.round(value / 1000) + 'k';
     }
-
     return `${value}`;
   }
 
   foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
   ];
 
   cars: Car[] = [
-    {value: 'volvo', viewValue: 'Volvo'},
-    {value: 'saab', viewValue: 'Saab'},
-    {value: 'mercedes', viewValue: 'Mercedes'},
+    { value: 'volvo', viewValue: 'Volvo' },
+    { value: 'saab', viewValue: 'Saab' },
+    { value: 'mercedes', viewValue: 'Mercedes' },
   ];
 }
