fc7a9bea10c23e89ae6596b1b9f047349634ffec
acatarinaoaraujo <acoa_@hotmail.com>
2025-02-22 16:28:42 -1000

    navbar with dropdown

--- eco-forest/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ eco-forest/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd1166e565a1ed9526d0f397dac0ae572fad4936")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece13739ad28f0f236c62b16f99492f3c7dafc3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
