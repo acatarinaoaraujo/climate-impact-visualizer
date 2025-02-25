45968a81c34f2136e3cc207dd578bb492f2e729c
acatarinaoaraujo <acoa_@hotmail.com>
2025-02-25 01:50:17 -1000

    transform geographical data

--- eco-forest/ClientApp/src/app/components/globe/globe.component.ts
+++ eco-forest/ClientApp/src/app/components/globe/globe.component.ts
@@ -56,85 +56,25 @@ export class GlobeComponent implements OnChanges {
     });
   }
 
+
   private transformData(geoJsonData: any, aggregatedData: any): any {
-    const countryMapping: { [key: string]: string } = {
-      'united states of america': 'united states',
-      'russia': 'russian federation',
-      'dominican republic': 'dominican rep.',
-      'hong kong': 'china, p.r.: hong kong',
-      'china': 'china, p.r.: mainland',
-      'taiwan': 'taiwan province of china',
-      'laos': 'lao people\'s dem. rep.',
-      'south korea': 'korea, dem. people\'s rep. of',
-      'north korea': 'korea, rep. of',
-      //  Timor-Leste, Dem. Rep. of
-      'east timor': 'timor-leste, dem. rep. of',
-      'brunei': 'brunei darussalam',
-      'fiji': 'fiji, rep. of',
-      'kazakhstan': 'kazakhstan, rep. of',
-      'kyrgyzstan': 'kyrgyz rep.',
-      'uzbekistan': 'uzbekistan, rep. of',
-      'tajikistan': 'tajikistan, rep. of',
-      'afghanistan': 'afghanistan, islamic rep. of',
-      'azerbaijan': 'azerbaijan, rep. of',
-      'iran': 'iran, islamic rep. of',
-      'syria': 'syrian arab rep.',
-      'turkey': 'türkiye, rep. of',
-      'egypt': 'egypt, arab rep. of',
-      'armenia': 'armenia, rep. of',
-      'yemen': 'yemen, rep. of',
-      'ethiopia': 'ethiopia, the federal dem. rep. of',
-      'eritrea': 'eritrea, the state of',
-      'tanzania': 'tanzania, united rep. of',
-      'democratic republic of the congo': 'congo, dem. rep. of the',
-      'central african republic': 'central african rep.',
-      'republic of the congo': 'congo, rep. of',
-      'mauritius': 'mauritius, islamic rep. of',
-      'madagascar': 'madagascar, rep. of',
-      'mozambique': 'mozambique, rep. of',
-      'equatorial guinea': 'equatorial guinea, rep. of',
-      'united republic of tanzania': 'tanzania, united rep. of',
-      'south sudan': 'south sudan, rep. of',
-      'mauritania': 'mauritania, islamic rep. of',
-      'gambia': 'gambia, the',
-      'lesotho': 'lesotho, kingdom of',
-      'estonia': 'estonia, rep. of',
-      'belarus': 'belarus, rep. of',
-      'poland': 'poland, rep. of',
-      'netherlands': 'netherlands, the',
-      'czechia': 'czech rep.',
-      'slovakia': 'slovak rep.',
-      'slovenia': 'slovenia, rep. of',
-      'croatia': 'croatia, rep. of',
-      'moldova': 'moldova, rep. of',
-      'republic of serbia': 'serbia, rep. of',
-      'kosovo': 'kosovo, republic of',
-      'macedonia': 'north macedonia, republic of',
-      'venezuela': 'venezuela, rep. bolivariana de',
-      'the bahamas': 'bahamas, the',
-      'falkland islands': 'falkland islands (malvinas)',
-
-    };
-
     const dataMap = new Map();
     aggregatedData.forEach((item: any) => {
-      const normalizedCountry = item.country.trim().toLowerCase();
-      dataMap.set(normalizedCountry, item);
+
+      const normalizedIso2 = item.isO2.trim().toLowerCase();
+      dataMap.set(normalizedIso2, item);
     });
 
     geoJsonData.features.forEach((feature: any) => {
-      let geoCountryName = feature.properties.ADMIN.trim().toLowerCase();
-      if (countryMapping[geoCountryName]) {
-        geoCountryName = countryMapping[geoCountryName]; // Map GeoJSON name to API name
-      }
-      if (dataMap.has(geoCountryName)) {
-        feature.properties.aggregatedData = dataMap.get(geoCountryName);
+      const geoIso2 = feature.properties.ISO_A2.trim().toLowerCase();
+      if (dataMap.has(geoIso2)) {
+        feature.properties.aggregatedData = dataMap.get(geoIso2);
       }
     });
 
-
     return geoJsonData;
   }
+
 
 
   private updateGlobeVisualization(): void {
