4bb6992144a2b65bba16346feaff3c65e61905dd
Ana Catarina O. Araújo <60526884+acatarinaoaraujo@users.noreply.github.com>
2025-03-26 21:53:29 -1000

    install packages

--- eco-forest/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ eco-forest/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c838dae4f77da98de98ae3e6d777900eefd5d54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3591de99a9f28c58f860dc59f637001e09bc4ed5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
