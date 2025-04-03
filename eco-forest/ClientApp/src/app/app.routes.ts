32e2dd68df7cd9be61f955156c7b416a14991a70
Ana Catarina O. Araújo <60526884+acatarinaoaraujo@users.noreply.github.com>
2025-04-03 00:07:45 -1000

    Revert "Reapply "sidebar urls""
    This reverts commit 02af2614afd8d99974651c80b94476935e266f8b.

--- eco-forest/ClientApp/src/app/app.routes.ts
+++ eco-forest/ClientApp/src/app/app.routes.ts
@@ -3,15 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EnergyGlobeComponent } from './components/globe/renewable-energy/energy-globe.component';
 import { DisasterGlobeComponent } from './components/globe/climate-disasters/disaster-globe.component';
-import { ForestGlobeComponent } from './components/globe/forest-carbon/forest-globe.component';
-import { IncomeGlobeComponent } from './components/globe/income-loss/income-globe.component';
-import { EmissionsGlobeComponent } from './components/globe/emissions/emissions-globe.component';
 
 export const routes: Routes = [
   { path: 'renewable-energy', component: EnergyGlobeComponent, data: { apiType: 'renewable-energy' } },
-  { path: 'income-loss', component: IncomeGlobeComponent, data: { apiType: 'income-loss' } },
-  { path: 'greenhouse-emissions', component: EmissionsGlobeComponent, data: { apiType: 'greenhouse-emissions' } },
-  { path: 'forest-carbon', component: ForestGlobeComponent, data: { apiType: 'forest-carbon' } },
+  { path: 'gdp-losses-benefits', redirectTo: 'https://climatedata.imf.org/pages/ngfs/#ngfs7', pathMatch: 'full' },
+  { path: 'greenhouse-emissions', redirectTo: 'https://climatedata.imf.org/pages/greenhouse-gas-emissions#gg4', pathMatch: 'full' },
+  { path: 'climate-weather/surface-temperature', redirectTo: 'https://climatedata.imf.org/pages/climate-and-weather#cc1', pathMatch: 'full' },
+  { path: 'climate-weather/mean-sea-levels', redirectTo: 'https://climatedata.imf.org/pages/climate-and-weather#cc3', pathMatch: 'full' },
+  { path: 'mitigation/forest-carbon', redirectTo: 'https://climatedata.imf.org/pages/mitigation#mi6', pathMatch: 'full' },
   { path: 'climate-disasters', component: DisasterGlobeComponent, data: { apiType: 'climate-disasters' } },
 ];
 
