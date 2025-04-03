3180a1f4dc86e6f8c7edc8506c3e79b2305ba59c
Ana Catarina O. Araújo <60526884+acatarinaoaraujo@users.noreply.github.com>
2025-04-03 00:10:03 -1000

    Reapply "sidebar urls"
    This reverts commit ac5163346ac55f8ccd19eddccfdb9a97ef468cc4.

--- eco-forest/ClientApp/src/app/app.component.ts
+++ eco-forest/ClientApp/src/app/app.component.ts
@@ -2,17 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';  
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { EnergyGlobeComponent } from './components/globe/renewable-energy/energy-globe.component';
-import { DisasterGlobeComponent } from './components/globe/climate-disasters/disaster-globe.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { Router, ActivatedRoute } from '@angular/router';
 
+import { EnergyGlobeComponent } from './components/globe/renewable-energy/energy-globe.component';
+import { DisasterGlobeComponent } from './components/globe/climate-disasters/disaster-globe.component';
+import { ForestGlobeComponent } from './components/globe/forest-carbon/forest-globe.component';
+import { IncomeGlobeComponent } from './components/globe/income-loss/income-globe.component';
+import { EmissionsGlobeComponent } from './components/globe/emissions/emissions-globe.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [CommonModule, EnergyGlobeComponent, DisasterGlobeComponent, SidebarComponent, NavbarComponent, HttpClientModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    EnergyGlobeComponent, 
+    DisasterGlobeComponent, 
+    ForestGlobeComponent, 
+    IncomeGlobeComponent, 
+    EmissionsGlobeComponent, 
+    SidebarComponent, 
+    NavbarComponent, 
+    HttpClientModule, 
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -23,22 +37,20 @@ export class AppComponent {
   endYear: number = 2025;
   selectedApiType: string = '';
 
-  // selectedIndicatorType = 'Climate related disasters frequency, Number of Disasters: Landslide';
-
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.url.subscribe(() => {
       const url = this.router.url;
-      this.selectedApiType = url.includes('climate-disasters') ? 'climate-disasters' : 'renewable-energy';
+      if (url.includes('renewable-energy')) this.selectedApiType = 'renewable-energy';
+      else if (url.includes('income-loss')) this.selectedApiType = 'income-loss';
+      else if (url.includes('greenhouse-emissions')) this.selectedApiType = 'greenhouse-emissions';
+      else if (url.includes('forest-carbon')) this.selectedApiType = 'forest-carbon';
+      else if (url.includes('climate-disasters')) this.selectedApiType = 'climate-disasters';
+      else this.selectedApiType = '';
     });
   }
   
-  // onIndicatorTypeChange(indicatorType: string) {
-  //   this.selectedIndicatorType = indicatorType;
-  // }
-
-
   onTypeChange(type: string) {
     this.selectedType = type;
   }
