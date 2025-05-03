9163aad9dd6bf0717398a3cf6f096595b94e5857
Ana Catarina O. Araújo <60526884+acatarinaoaraujo@users.noreply.github.com>
2025-05-02 21:42:47 -1000

    Revert "update years"
    This reverts commit 8b24a446e07c5c4d9cd9ce153d809d4320e094ee.

--- eco-forest/ClientApp/src/app/app.component.ts
+++ eco-forest/ClientApp/src/app/app.component.ts
@@ -1,115 +1,102 @@
-// app.component.ts
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';  
+import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { MatDialogModule } from '@angular/material/dialog';
-import { filter } from 'rxjs/operators';
-
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { API_YEAR_RANGE } from './shared/constants';
 
 import { EnergyGlobeComponent } from './components/globe/renewable-energy/energy-globe.component';
 import { DisasterGlobeComponent } from './components/globe/climate-disasters/disaster-globe.component';
 import { ForestGlobeComponent } from './components/globe/forest-carbon/forest-globe.component';
 import { IncomeGlobeComponent } from './components/globe/income-loss/income-globe.component';
 import { EmissionsGlobeComponent } from './components/globe/emissions/emission-globe.component';
+import { ENERGY_TYPES, INCOME_TYPES, EMISSIONS_TYPES, FOREST_TYPES, DISASTER_TYPES } from './shared/constants';
 
-import {
-  API_YEAR_RANGE,
-  ENERGY_TYPES,
-  INCOME_TYPES,
-  EMISSIONS_TYPES,
-  FOREST_TYPES,
-  DISASTER_TYPES
-} from './shared/constants';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: true, 
   imports: [
-    CommonModule,
-    HttpClientModule,
+    CommonModule, 
+    EnergyGlobeComponent, 
+    DisasterGlobeComponent, 
+    ForestGlobeComponent, 
+    IncomeGlobeComponent, 
+    EmissionsGlobeComponent, 
+    SidebarComponent, 
+    NavbarComponent, 
+    HttpClientModule, 
     RouterOutlet,
-    MatDialogModule,
-    SidebarComponent,
-    NavbarComponent,
-    EnergyGlobeComponent,
-    DisasterGlobeComponent,
-    ForestGlobeComponent,
-    EmissionsGlobeComponent,
-    IncomeGlobeComponent
+    MatDialogModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  apiType!: string;
-  indicatorType!: string;
-  startYear!: number;
-  endYear!: number;
-  selectedYear!: number;
+  title = 'client-app';
+  indicatorType: string = '';
+  startYear: number = 2000;
+  endYear: number = 2025;
+  selectedYear: number = 2023;
+  apiType: string = '';
 
-  constructor(private router: Router) {
-    // Initial setup before first CD
-    this.updateParams(this.router.url);
-
-    // Re-apply on every NavigationEnd
-    this.router.events
-      .pipe(filter(evt => evt instanceof NavigationEnd))
-      .subscribe((evt: NavigationEnd) => {
-        this.updateParams(evt.urlAfterRedirects);
-      });
+  constructor(private router: Router, private route: ActivatedRoute) {
+    console.log('AppComponent initialized');
   }
 
-  private updateParams(url: string) {
-    if (url.includes('renewable-energy')) {
-      this.apiType       = 'renewable-energy';
-      this.indicatorType = ENERGY_TYPES[0];
-    } else if (url.includes('income-loss')) {
-      this.apiType       = 'income-loss';
-      this.indicatorType = INCOME_TYPES[0];
-    } else if (url.includes('greenhouse-emissions')) {
-      this.apiType       = 'greenhouse-emissions';
-      this.indicatorType = EMISSIONS_TYPES[0];
-    } else if (url.includes('forest-carbon')) {
-      this.apiType       = 'forest-carbon';
-      this.indicatorType = FOREST_TYPES[0];
-    } else if (url.includes('climate-disasters')) {
-      this.apiType       = 'climate-disasters';
-      this.indicatorType = DISASTER_TYPES[0];
-    } else {
-      this.apiType       = '';
-      this.indicatorType = '';
-    }
-
-    if (this.apiType && this.apiType in API_YEAR_RANGE) {
-      const r = API_YEAR_RANGE[this.apiType as keyof typeof API_YEAR_RANGE];
-      this.startYear    = r.min;
-      this.endYear      = r.max;
-      this.selectedYear = r.max;
-    } else {
-      this.startYear    = 2000;
-      this.endYear      = 2025;
-      this.selectedYear = 2025;
-    }
+  ngOnInit() {
+    this.route.url.subscribe(() => {
+      const url = this.router.url;
+  
+      if (url.includes('renewable-energy')) {
+        this.apiType = 'renewable-energy';
+        this.indicatorType = ENERGY_TYPES[0];        // ← Fossil Fuels
+      } else if (url.includes('income-loss')) {
+        this.apiType = 'income-loss';
+        this.indicatorType = INCOME_TYPES[0];
+      } else if (url.includes('greenhouse-emissions')) {
+        this.apiType = 'greenhouse-emissions';
+        this.indicatorType = EMISSIONS_TYPES[0];
+      } else if (url.includes('forest-carbon')) {
+        this.apiType = 'forest-carbon';
+        this.indicatorType = FOREST_TYPES[0];
+      } else if (url.includes('climate-disasters')) {
+        this.apiType = 'climate-disasters';
+        this.indicatorType = DISASTER_TYPES[0];
+      } else {
+        this.apiType = '';
+        this.indicatorType = '';
+      }
+  
+      const yearRange = this.apiType in API_YEAR_RANGE
+        ? API_YEAR_RANGE[this.apiType as keyof typeof API_YEAR_RANGE]
+        : { min: 2000, max: 2025 };
+  
+      this.startYear = yearRange.min;
+      this.endYear   = yearRange.max;
+    });
   }
 
+  
+  onIndicatorTypeChange(newIndicator: string) {
+    console.log('Indicator Type Changed:', newIndicator);
+    this.indicatorType = newIndicator;
+  }
+
+  onYearRangeChange(yearRange: { startYear: number; endYear: number }) {
+    this.startYear = yearRange.startYear;
+    this.endYear = yearRange.endYear;
+  }
 
   onApiTypeChange(type: string) {
-    this.router.navigate([type]);
+    this.apiType = type;
   }
 
-  onIndicatorTypeChange(newInd: string) {
-    this.indicatorType = newInd;
-  }
-  onYearRangeChange(range: { startYear: number; endYear: number }) {
-    this.startYear = range.startYear;
-    this.endYear   = range.endYear;
-    if (this.selectedYear < this.startYear) this.selectedYear = this.startYear;
-    if (this.selectedYear > this.endYear)   this.selectedYear = this.endYear;
-  }
   onSelectedYearChange(year: number) {
+    console.log('Selected Year Changed:', year);
     this.selectedYear = year;
   }
 }
