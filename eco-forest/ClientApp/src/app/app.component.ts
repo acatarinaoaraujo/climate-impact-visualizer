373d474d236bb42f30a0dfeabaa0d139dad1bb8e
Ana Catarina O. Araújo <60526884+acatarinaoaraujo@users.noreply.github.com>
2025-05-02 21:22:39 -1000

    update indicator type

--- eco-forest/ClientApp/src/app/app.component.ts
+++ eco-forest/ClientApp/src/app/app.component.ts
@@ -12,6 +12,7 @@ import { DisasterGlobeComponent } from './components/globe/climate-disasters/dis
 import { ForestGlobeComponent } from './components/globe/forest-carbon/forest-globe.component';
 import { IncomeGlobeComponent } from './components/globe/income-loss/income-globe.component';
 import { EmissionsGlobeComponent } from './components/globe/emissions/emission-globe.component';
+import { ENERGY_TYPES, INCOME_TYPES, EMISSIONS_TYPES, FOREST_TYPES, DISASTER_TYPES } from './shared/constants';
 
 import { MatDialogModule } from '@angular/material/dialog';
 
@@ -36,34 +37,50 @@ import { MatDialogModule } from '@angular/material/dialog';
 })
 export class AppComponent {
   title = 'client-app';
-  indicatorType: string = 'Share Of Forest Area';
+  indicatorType: string = '';
   startYear: number = 2000;
   endYear: number = 2025;
   selectedYear: number = 2023;
   apiType: string = '';
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute) {
+    console.log('AppComponent initialized');
+  }
 
   ngOnInit() {
     this.route.url.subscribe(() => {
       const url = this.router.url;
-      if (url.includes('renewable-energy')) this.apiType = 'renewable-energy';
-      else if (url.includes('income-loss')) this.apiType = 'income-loss';
-      else if (url.includes('greenhouse-emissions')) this.apiType = 'greenhouse-emissions';
-      else if (url.includes('forest-carbon')) this.apiType = 'forest-carbon';
-      else if (url.includes('climate-disasters')) this.apiType = 'climate-disasters';
-      else this.apiType = '';
-
-      // After setting apiType, update the year range based on the selected API type
+  
+      if (url.includes('renewable-energy')) {
+        this.apiType = 'renewable-energy';
+        this.indicatorType = ENERGY_TYPES[0];        // ← Fossil Fuels
+      } else if (url.includes('income-loss')) {
+        this.apiType = 'income-loss';
+        this.indicatorType = INCOME_TYPES[0];
+      } else if (url.includes('greenhouse-emissions')) {
+        this.apiType = 'greenhouse-emissions';
+        this.indicatorType = EMISSIONS_TYPES[0];
+      } else if (url.includes('forest-carbon')) {
+        this.apiType = 'forest-carbon';
+        this.indicatorType = FOREST_TYPES[0];
+      } else if (url.includes('climate-disasters')) {
+        this.apiType = 'climate-disasters';
+        this.indicatorType = DISASTER_TYPES[0];
+      } else {
+        this.apiType = '';
+        this.indicatorType = '';
+      }
+  
       const yearRange = this.apiType in API_YEAR_RANGE
-  ? API_YEAR_RANGE[this.apiType as keyof typeof API_YEAR_RANGE]
-  : { min: 2000, max: 2025 };
-
-
+        ? API_YEAR_RANGE[this.apiType as keyof typeof API_YEAR_RANGE]
+        : { min: 2000, max: 2025 };
+  
       this.startYear = yearRange.min;
-      this.endYear = yearRange.max;
+      this.endYear   = yearRange.max;
     });
   }
+
+  
   onIndicatorTypeChange(newIndicator: string) {
     console.log('Indicator Type Changed:', newIndicator);
     this.indicatorType = newIndicator;
